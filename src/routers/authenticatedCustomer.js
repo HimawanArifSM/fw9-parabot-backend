b9ea08ddbae26e3d48d58392df625224f2e7472e
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-09-19 11:41:49 +0700

    fix: auth customer

--- src/routers/authenticatedCustomer.js
+++ src/routers/authenticatedCustomer.js
@@ -1,10 +1,14 @@
+const authenticatedCustomer = require('express').Router();
+const authenticatedControl = require('../controllers/authenticated')
 const authMiddleware = require('../middleware/auth');
 
-// get data costumer
-// edit profile & email
-// add to cart
+authenticatedCustomer.get('/profile/customer',authMiddleware,authenticatedControl.getProfileSeller)// get data costumer
+authenticatedCustomer.patch('/profile/customer',authMiddleware,authenticatedControl.updateProfileSeller)// edit profile & email
+authenticatedCustomer.patch('/profile/customer/email',authMiddleware,authenticatedControl.updateEmail)// update email
 // add to wishlist
 // add to favorit
 // delete cart by id
 // checkout 'order payment'
 // get all order
+
+module.exports=authenticatedCustomer
